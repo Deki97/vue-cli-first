15731301782be4e4a2391fef91ec69a6941a4ee2
Deki97 <dejan97@libero.it>
2021-12-17 17:04:10 +0100

    bonus banner done

---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <h2>Sono un componente che visualizza un Jumbotron!</h2>
+        <img src="../assets/img/ocean.jpg" alt="Los Angeles">
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Hero"
+}
+</script>
