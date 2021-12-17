a7184ca89c1cbdd57bebe90dd0f7a3a4f17c2149
Deki97 <dejan97@libero.it>
2021-12-17 17:05:30 +0100

    bonus fatto

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <h2>Sono un componente che visualizza un Jumbotron!</h2>
-        <img src="../assets/img/ocean.jpg" alt="Los Angeles">
+        <img src="../assets/img/ocean.jpg" alt="Ocean">
     </section>
 </template>
 
